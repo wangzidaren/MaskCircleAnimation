e645cfaaa3a3e66d5f57462b295da30f8985ea08
tracy <mac@macmatoMacBook-Pro.local>
2015-01-10 17:01:29 +0800

    Initial Commit

---
+++ MaskCircleAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaskCircleAnimation
+//
+//  Created by tracy on 15-1-10.
+//  Copyright (c) 2015年 tracy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MaskCircleAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaskCircleAnimation
+//
+//  Created by tracy on 15-1-10.
+//  Copyright (c) 2015年 tracy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
